1600444c60a80739cdf6a12f193873b82947612e
root <crypto0524@gmail.com>
2018-04-20 10:14:12 +0900

    moved all stuff to github

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -32,7 +32,7 @@ const size_t   CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE        = 600;
 //TODO Define number of digits
 const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 8;
 //TODO Define minimum fee for transactions
-const uint64_t MINIMUM_FEE                                   = 100;
+const uint64_t MINIMUM_FEE                                   = 0.1;
 const uint64_t DEFAULT_DUST_THRESHOLD                        = MINIMUM_FEE;
 
 //TODO Define preferred block's target time
